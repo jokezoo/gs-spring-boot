d0c5ccdcab4efbae1b767fd78baffd7b651fd847
effyma <effyma1@gmail.com>
2018-06-05 18:00:23 +0800

    add apache commons

--- springmvc/src/main/java/springmvc/TestMaint.java
+++ springmvc/src/main/java/springmvc/TestMaint.java
@@ -12,19 +12,7 @@ import com.google.gson.Gson;
 public class TestMaint {
 
 	public static void main(String[] args) {
-		ArrayList<String> colorList= new ArrayList<String>();
-		colorList.add(null);
-		colorList.add("FFFFF0");
-		colorList.add("FFFFF1");
-		colorList.add("FFFFF2");
-		String json = new Gson().toJson(colorList);
-		System.out.println("from arraylist to json: "+ json);
-		ArrayList<String> colorList2 = new ArrayList<String>();
-		colorList2 = new Gson().fromJson(json, ArrayList.class);
-		for(String c : colorList2) {
-			System.out.println("from json to arraylist: "+ c);
-		}
-		
+
 	}
 
 }
